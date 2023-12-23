04819c1d430862d1f0e578bd7d5c11cef2b38212
Daniel Wilcox <danielgwilcox@gmail.com>
2023-12-22 21:56:39 -0500

    Update App format

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import PageContainer from "./components/pages/PageContainer";
+import Navbar from "./components/navbar/Navbar";
+import { Pages } from "./classes/pages";
+import { useState } from "react";
 
 function App() {
+  const beachImage = "./images/beach_4.jpg";
+  const [page, setPage] = useState(Pages.ABOUT);
 
   return (
     <>
-      <div>
-        <h1>
-          Hello World
-        </h1>
+      <div
+        className="flex flex-col text-black text-center  justify-center align-middle h-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${beachImage})` }}
+      >
+        <Navbar setPage={setPage} currentPage={page}></Navbar>
+        <PageContainer page={page}></PageContainer>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
